445acfc226ccd381a7f678e4ae0bf1569ddf7977
Khalil <khalilpati55@gmail.com>
2024-08-21 02:15:44 +0530

    Setup Supabase and generated type of the database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+import { createClient } from "@/lib/supabase/server";
+
+export default async function Home() {
+  const supabase = createClient();
+  const { data } = await supabase.from("urls").select("*");
+  console.log(data);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold">Hello World</h1>
